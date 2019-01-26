d99b22492d63872d2c53a6a6f5aeb741517e2fd5
Carina Cruz <lecruz@bu.edu>
2019-01-26 19:40:22 +0000

    updates

--- dropit.js
+++ dropit.js
@@ -1,6 +1,15 @@
 function dropElements(arr, func) {
-  // Drop them elements.
+  // drop them elements.
+  var times = arr.length;
+  for (var i = 0; i < times; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
   return arr;
 }
 
-dropElements([1, 2, 3], function(n) {return n < 3; });
+// test here
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;})
---
+++ mapthedebris.js
@@ -0,0 +1,35 @@
+var math; 
+var PI; 
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr;
+}
+
+var newArray= []; //Create a new array for the name,orbital
+
+var T = 2 * math.PI; //multiply pi by 2
+
+var determineOrbital = function(object) {
+//   var U = Radius**AvgAlt; //radius and altitude to the 3rd power
+var U = math.pow(radius + AvgAlt * 3);
+var V = U/GM;  //(r+AA/GM of Earth)
+
+ //use math.round as the last step
+ //delete satelite
+};
+
+
+//Getting the orbital
+//multiply pi by 2
+//Get the sqrt of r + AA cubed divided by GM
+
+
+var sqrt = math.sqrt(6367+AA);
+
+
+
+
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
